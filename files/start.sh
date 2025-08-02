e0274d4993b5f13fa5329a67b1b8ef1eb4e4dcf1
Vladimir Sakhonchik <vladimir.sakhonchik.ext@tvh.com>
2025-08-02 20:47:07 +0200

    change start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,31 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 set -e
 
-cat <<'MSG'
-🧪 Welcome to Linux CLI Training
-
-Your task:
-  🔹 Locate the application configuration file
-  🔹 Change the port number to the correct one
-  🔹 Fix the file permissions → modify the port → (optionally) revert permissions
-  🔹 After editing, use: systemctl restart db
-
-Bonus commands:
-  • systemctl start db      → Start the service
-  • systemctl stop db       → Stop it
-  • systemctl status db     → Check if it’s running
-  • systemctl restart db    → Restart with updated config
-
-Hints:
-  • Find the config file: db.conf
-  • Check the config file permissions
-  • Test it: curl http://localhost:5432
-
-📘 Note: The server must be running on correct port to return a secret sentence.
-
-Good luck!
-MSG
-
-# Start interactive shell
 exec bash -l
+
 
